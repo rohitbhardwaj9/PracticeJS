f349279fda77241b873d88b065a552a4aa1f8256
Rohit Bhardwaj <me.rohitbhardwaj9@gmail.com>
2023-05-17 14:12:40 +0000

    initial commit

--- index.js
+++ index.js
@@ -43,7 +43,47 @@ getSubstring(str_val);
 */
 
 // 6.) substr(start, length)
+/*
 function getSubstr(str){
   console.log(str.substr("11", "4"));
 }
 getSubstr(str_val);
+*/
+
+// 7.) replace(replace_value, replace_with_value) example
+/*
+function getReplace(str){
+  console.log(str.replace("Geeks", "students"));
+}
+getReplace(str_val);
+*/
+
+// 8.) replaceAll(regex | substring, newSubstr | funtion)
+/*
+function getReplaceAll(str){
+  console.log(str.replaceAll("Geeks", "Geek"));
+}
+getReplaceAll(str_val);
+*/
+
+// 9.) toUpperCase(string variable) Example
+/*
+function getUpperCase(str){
+  console.log(str.toUpperCase(str));
+}
+getUpperCase(str_val);
+*/
+
+// 10.) toLowerCase(string variable) Example
+/*
+function getLowerCase(str){
+  console.log(str.toLowerCase(str));
+}
+getLowerCase(str_val);
+*/
+
+// 11.) concat() Example
+function getConcatValue(str){
+  console.log(str.concat(" this year"));
+}
+getConcatValue(str_val);
